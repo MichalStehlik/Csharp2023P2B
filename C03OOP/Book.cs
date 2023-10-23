54d61c9ead138db785576acbd1e7f95a470fa6bc
Michal Stehlík <michal.stehlik@pslib.cz>
2023-10-23 08:44:32 +0200

    oop

---
+++ C03OOP/Book.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C03OOP
+{
+    internal class Book // třída
+    {
+        private string name; // atribut, datový člen
+        private string _author; // vhodná konvence
+
+        public string GetName() // metoda
+        {
+            return name;
+        }
+
+        public void SetName(string name)
+        {
+            if (name.Length < 2)
+            {
+                this.name = name;
+            }
+        }
+
+        public string GetAuthor() // metoda
+        {
+            return _author;
+        }
+
+        public void SetAuthor(string author)
+        {
+            _author = author;
+        }
+
+        public string GetTitle()
+        {
+            return _author + ": " + name;
+        }
+        // implicitní konstruktor
+        public Book(string name, string author = "?") // parametrický konstruktor
+        {
+            //this.name = name;
+            //_author = author;
+            SetName(name);
+            SetAuthor(author);
+        }
+        /*
+        public Book(string name) // parametrický konstruktor, nahrazený implicitními parametry
+        {
+            this.name = name;
+            _author = "?";
+        }
+        */
+        public Book() // bezparametrický konstruktor
+        {
+            this.name = "?";
+            _author="?";
+        }
+
+        ~Book() // desktruktor
+        { 
+            // do something
+        }
+    }
+}
---
+++ C03OOP/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using C03OOP;
+
+Book pp = new Book("Pán prstenů");
+//Console.WriteLine(pp.name);
+Console.WriteLine(pp.GetName());
+pp.SetName("Společenstvo prstenů");
+Console.WriteLine(pp.GetName());
+
+Book tt = new Book();
+Console.WriteLine(tt.GetName());
+Console.WriteLine(tt.GetTitle());
+
+Book[] books = new Book[2];
+books[0] = pp;
+books[1] = tt;
+foreach(var b in books)
+{
+    Console.WriteLine(b.GetName());
+}
+/*
+string[] names =  new string[2];
+string[] authors = new string[2];
+authors[0] = "JRR";
+authors[1] = "JRR";
+*/
