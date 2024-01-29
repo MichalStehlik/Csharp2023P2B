2851dd7038fd3bf2e563842193e1caf017f7123f
Michal Stehlík <michal.stehlik@pslib.cz>
2024-01-29 08:42:25 +0100

    delegates

---
+++ C12Extension/Delegates.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C12Extension
+{
+    internal class Delegates
+    {
+        public Delegates(float a, float b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public float Do(MathOperation mop)
+        {
+            return mop(A, B);
+        }
+
+        // e => x + 1; // lambda // arrow function
+        public float Addition(float a, float b) { return a + b; } // MathOperation
+        public float Subtraction(float a, float b) { return a - b; }
+        public float Multiplication(float a, float b) { return a * b; }
+        public float Division(float a, float b) { return a / b; }
+        public float A { get; set; }
+        public float B { get; set; }
+        public float C { get; set; }
+    }
+
+    public delegate float MathOperation(float a, float b);
+    // všechy funkce, které mají na vstupu dva float a vrací float
+}
---
+++ C12Extension/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using C12Extension;
+using System.Linq;
+
+var del = new Delegates(7,1);
+Console.WriteLine(del.Do(del.Addition));
+Console.WriteLine(del.Do(del.Subtraction));
+Console.WriteLine(del.Do(del.Multiplication));
+Console.WriteLine(del.Do(del.Division));
+Console.WriteLine(del.Do(ReverseSubtraction));
+Console.WriteLine(del.Do(delegate (float x, float y) { return x * x * y; }));
+Console.WriteLine(del.Do((x, y) => { return x * x * y; }));
+Console.WriteLine(del.Do((x, y) => ( x * x * y )));
+Console.WriteLine();
+
+List<int> floats = new List<int> { 1,10,3,4,40,6,7,8,2,1 };
+foreach(var x in floats.Where(y => y > 16))
+{
+    Console.WriteLine(x);
+}
+foreach (var x in floats.OrderBy(y => y))
+{
+    Console.WriteLine(x);
+}
+Console.WriteLine();
+foreach (var x in floats
+    .Where(y => (y % 2 == 0))
+    .OrderByDescending(y => y))
+{
+    Console.WriteLine(x);
+}
+
+float ReverseSubtraction(float a, float b) { return b - a; }
