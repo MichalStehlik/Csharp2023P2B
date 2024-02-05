c460e0602d0faec57637a79263c87310fdf5a89c
Michal Stehlík <michal.stehlik@pslib.cz>
2024-02-05 08:42:32 +0100

    extension methods

---
+++ C15Extension/ListExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace C15Extension
+{
+    internal static class ListExtensions
+    {
+        public static int OddCount(this List<int> data) 
+        {
+            int i = 0;
+            foreach (var item in data)
+            {
+                if (item % 2 == 1)
+                {
+                    i++;
+                }
+            }
+            return i;
+        }
+
+        /*
+        public static int OddCount(List<int> data)
+        {
+            int i = 0;
+            foreach (var item in data)
+            {
+                if (item % 2 == 1)
+                {
+                    i++;
+                }
+            }
+            return i;
+        }
+        */
+        public static int GreaterThen(this List<int> data, int value)
+        {
+            int i = 0;
+            foreach (var item in data)
+            {
+                if (item > value)
+                {
+                    i++;
+                }
+            }
+            return i;
+        }
+
+        public static int ItemsCount<T>(this List<T> data) 
+        { 
+            return data.Count; 
+        }
+
+        public static List<int> Odd(this List<int> data)
+        {
+            var list = new List<int>();
+            foreach (var item in data)
+            {
+                if (item % 2 == 1) { list.Add(item); }
+            }
+            return list;
+        }
+
+        public static List<string> LongerThen(this List<string> data, int length)
+        {
+            var list = new List<string>();
+            foreach (var item in data)
+            {
+                if (item.Length > length) { list.Add(item); }
+            }
+            return list;
+        }
+    }
+}
---
+++ C15Extension/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using C15Extension;
+using System.Linq;
+
+List<int> ints = new List<int> { 1, 2, 3, 7, 12, 89, 1, 9, 3 };
+BetterList ints2 = new BetterList { 1, 2, 3, 7, 12, 89, 1, 9, 3 };
+List<string> names = new List<string> { "Adéla", "Bořivoj", "Ctimír", "David", "Evžen"};
+//Console.WriteLine(ListExtensions.OddCount(ints));
+Console.WriteLine(ints.OddCount());
+Console.WriteLine(ints.GreaterThen(10));
+Console.WriteLine(ints.ItemsCount());
+Console.WriteLine(names.ItemsCount());
+Console.WriteLine("");
+foreach (int i in ints.Odd())
+{
+    Console.WriteLine(i);
+}
+foreach (string i in names.LongerThen(5))
+{
+    Console.WriteLine(i);
+}
+foreach (string i in names.Where(x => x.Length > 5))
+{
+    Console.WriteLine(i);
+}
