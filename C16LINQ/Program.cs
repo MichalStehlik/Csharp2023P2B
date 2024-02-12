f9836bc2b2ae9cda0bac0c1ad90fe9f075c44ac9
Michal Stehlík <michal.stehlik@pslib.cz>
2024-02-12 08:51:40 +0100

    xxxxxxxx

---
+++ C16LINQ/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+List<Human> humans = new List<Human> {
+    new Human { Firstname = "Alois", Lastname="Antl"},
+    new Human { Firstname = "Beáta", Lastname="Blažejová"},
+    new Human { Firstname = "Cyril", Lastname="Cvach"},
+    new Human { Firstname = "Diana", Lastname="Drahá"},
+    new Human { Firstname = "Evžen", Lastname="Evil"}
+};
+
+foreach (Human human in humans)
+{
+    Console.WriteLine(human);
+}
+
+class Human
+{
+    public string Firstname { get; set; } = string.Empty;
+    public string Lastname { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+
+    publi
+}
